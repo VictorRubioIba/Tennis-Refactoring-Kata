85608ebe832db28c628196cf9cd9ade87a68c3cb
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-04 16:46:55 +0100

    refactor: delete variable

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,10 +3,8 @@ public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
